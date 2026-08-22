08f771327a6963ca3c1ada5713ae77a4a48270f7
Emdien <gonnm8@gmail.com>
2026-08-22 20:46:49 +0200

    adding flags
    unfinished

--- main.go
+++ main.go
@@ -5,11 +5,19 @@ import (
 	"net/http"
 	"log"
 	"github.com/Emdien/simple-server-auth/server"
+	"flag"
 )
 
 func main() {
 
 	hmacSecret := make([]byte, 32)
+
+	portPtr := flag.String("port", ":8088", "Server port it will be listening to")
+	tokenDurationHour := flag.Int("hours", 1, "Duration of token in hours. Larger unit takes precedence. Does not sum.")
+	tokenDurationMinutes := flag.Int("minutes", 0, "Duration of token in minutes. Larger unit takes precedence. Does not sum.")
+	tokenDurationSeconds := flag.Int("seconds", 0, "Duration of token in seconds. Larger unit takes precedence. Dooes not sum.")
+	secretPtr := flag.String("secret", "", "HMAC Secret to use in token creation. If none passed, it will create one in runtime. (Volatile)")
+	verbosePtr := flag.Bool("verbose", false, "")
 
 	if _, err := rand.Read(hmacSecret); err != nil {
 		panic(err)
@@ -18,13 +26,5 @@ func main() {
 
 	srv := server.NewServer(hmacSecret)
     log.Fatal(http.ListenAndServe(":8088", srv.Routes()))
-	// 1. Define a server with the routes to auth and validate token
 
-	// 2. Implement handlers
-
-	// 3. Implement a function that leverages PAM to check with user and password if its valid
-	// If its valid, register the user in memory with a struct with IP, User and a private token that together with the token of the session creates a valid session
-	// this needs to be researched, its kinda like pub keys
-
-	// Could also research JWT tokens
 }
